2e3cbbfb9e1f6ee72019bd1a001d0893a09519b7
Chathura J Ekanayake <chathuraje01@gmail.com>
2025-05-01 12:37:05 +0530

    🍼 initial next.js commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        <div className="flex items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold">Hello, Next.js!</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
